c13532f34a6e0869079b9b6e391b222e7ea67e78
Joaquim <joaquin.quirosa@insbaixcamp.cat>
2026-04-24 14:33:35 +0200

    feat: implement Prestec class with properties and methods for book lending

--- src/model/Prestec.java
+++ src/model/Prestec.java
@@ -1,5 +1,44 @@
 package model;
 
+import java.time.LocalDate;
 public class Prestec {
 
-}
+    // Propietats
+    private Usuari usuari;
+    private Llibre llibre;
+    private LocalDate dataPrestec;
+    private LocalDate dataRetorn;
+
+
+    private static final int MAX_LLIBRES = 3;
+
+    public String prestecs(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
+
+        // Control del màxim de llibres
+        if (usuari.getLlibresPrestats().size() >= MAX_LLIBRES) {
+            return "L'usuari ha arribat al màxim de llibres permesos.";
+        }
+
+    // Constructor
+    public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
+        this.usuari = usuari;
+        this.llibre = llibre;
+        this.dataPrestec = dataPrestec;
+        this.dataRetorn = dataPrestec.plusWeeks(2);
+    }
+
+    public Usuari getUsuari() {
+        return usuari;
+    }
+
+    public Llibre getLlibre() {
+        return llibre;
+    }
+
+    public LocalDate getDataRetorn() {
+        return dataRetorn;
+    }
+    
+    
+
+} 
