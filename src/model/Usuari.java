ba96e9cfb2b446ba9121912f0053867862f0b063
Stageddat <hello@stageddat.dev>
2026-05-10 01:09:51 +0200

    refactor: remove unused methods and clean up setNom documentation in Usuari class

--- src/model/Usuari.java
+++ src/model/Usuari.java
@@ -16,6 +16,15 @@ public class Usuari {
     return nom;
   }
 
+  /**
+   * Modificar el nom del usuari.
+   * 
+   * @param nom El nou nom de l'usuari.
+   */
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
   public List<Llibre> getLlibresPrestats() {
     return llibresPrestats;
   }
@@ -31,69 +40,6 @@ public class Usuari {
         return;
       }
     }
-  }
-
-  /**
-   * Modificar el nom d'un usuari.
-   * 
-   * @param nom El nou nom de l'usuari.
-   */
-
-  public void setNom(String nom) {
-    this.nom = nom;
-  }
-
-  // LlISTAR User
-  /**
-   * Llistar les dades d'un usuari, incloent el seu nom i els llibres que té
-   */
-
-  public void llistarUsuari() {
-    System.out.println("Nom: " + nom);
-    System.out.print("Llibres prestats: ");
-    for (Llibre llibre : llibresPrestats) {
-      System.out.println(llibre.getTitol() + "- ");
-    }
-  }
-
-  /**
-   * Cercar un usuari per nom en una llista d'usuaris.
-   * 
-   * @param usuaris Llista d'usuaris on buscar.
-   * @param nom     Nom de l'usuari a cercar.
-   * @return L'usuari trobat o null si no s'ha trobat.
-   */
-  public static Usuari cercarUsuari(List<Usuari> usuaris, String nom) {
-    for (Usuari usuari : usuaris) {
-      if (usuari.getNom().equalsIgnoreCase(nom)) {
-        return usuari;
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Eliminar un usuari de la llista d'usuaris.
-   * 
-   * @param usuaris Llista d'usuaris de la qual eliminar.
-   * @param nom     Nom de l'usuari a eliminar.
-   * @return true si s'ha eliminat l'usuari, false si no s'ha trobat o té llibres
-   *         prestats.
-   */
-  public static boolean eliminarUsuari(List<Usuari> usuaris, String nom) {
-    for (Usuari usuari : usuaris) {
-      if (usuari.getNom().equalsIgnoreCase(nom)) {
-        if (usuari.getLlibresPrestats().isEmpty()) {
-          usuaris.remove(usuari);
-          return true;
-        } else {
-          System.out.println("No es pot eliminar l'usuari " + nom + " perquè té llibres prestats.");
-          return false;
-        }
-      }
-    }
-    System.out.println("No s'ha trobat l'usuari " + nom + ".");
-    return false;
   }
 
   @Override
