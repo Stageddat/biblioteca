8af5be5a971fda6ab823cec4c70c066e924e07d2
Stageddat <hello@stageddat.dev>
2026-05-10 01:12:17 +0200

    style: format code for consistency in GestorBiblioteca class

--- src/business/GestorBiblioteca.java
+++ src/business/GestorBiblioteca.java
@@ -9,46 +9,45 @@ import model.Usuari;
 import model.Prestec;
 
 public class GestorBiblioteca {
+  private List<Prestec> prestecs;
 
-    private List<Prestec> prestecs;
+  public GestorBiblioteca() {
+    this.prestecs = new ArrayList<>();
+  }
 
-    public GestorBiblioteca() {
-        this.prestecs = new ArrayList<>();
+  /**
+   * Prestar un llibre a un usuari.
+   * 
+   * @param usuari
+   * @param llibre
+   */
+  public void prestarLlibre(Usuari usuari, Llibre llibre) {
+    if (!llibre.esPrestat()) {
+      llibre.prestar();
+      Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
+      prestecs.add(prestec);
+      usuari.afegirLlibre(llibre);
+      System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+    } else {
+      System.out.println("Aquest llibre ja està prestat.");
     }
+  }
 
-    /**
-     * Prestar un llibre a un usuari.
-     * 
-     * @param usuari
-     * @param llibre
-     */
-    public void prestarLlibre(Usuari usuari, Llibre llibre) {
-        if (!llibre.esPrestat()) {
-            llibre.prestar();
-            Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
-            prestecs.add(prestec);
-            usuari.afegirLlibre(llibre);
-            System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
-        } else {
-            System.out.println("Aquest llibre ja està prestat.");
-        }
+  /**
+   * Controlar stock de llibres disponibles. (si tenim més d'un llibre)
+   * 
+   * @param usuari Nom de l'usuari que vol agafar el llibre.
+   * @param llibre El llibre que l'usuari vol agafar.
+   * @return
+   */
+  public static boolean controlStock(Usuari usuari, Llibre llibre) {
+    if (!llibre.esPrestat()) {
+      System.out.println("El llibre " + llibre.getTitol() + " està disponible per a " + usuari.getNom());
+      return true;
     }
-
-    /**
-     * Controlar stock de llibres disponibles. (si tenim més d'un llibre)
-     * 
-     * @param usuari Nom de l'usuari que vol agafar el llibre.
-     * @param llibre El llibre que l'usuari vol agafar.
-     * @return
-     */
-    public static boolean controlStock(Usuari usuari, Llibre llibre) {
-        if (!llibre.esPrestat()) {
-            System.out.println("El llibre " + llibre.getTitol() + " està disponible per a " + usuari.getNom());
-            return true;
-        }
-        System.out.println("El llibre " + llibre.getTitol() + " no està disponible per a " + usuari.getNom()
-                + " perquè ja està prestat.");
-        return false;
-    }
+    System.out.println("El llibre " + llibre.getTitol() + " no està disponible per a " + usuari.getNom()
+        + " perquè ja està prestat.");
+    return false;
+  }
 
 }
